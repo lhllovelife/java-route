6d178188a4026a449c9e6cc122bfb032298cdad2
lhllovezj <lhllovezj@gmail.com>
2020-03-31 01:44:36 +0800

    提交3.29创建的文件

---
+++ JavaSE(老杜)/跟课代码/Hello.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Hello {
+	public static void main(String[] args){
+		System.out.print("Hello");
+	}
+}
+
+
+// import java.util.Scanner;
+
+// public class Hello {
+    // public static void main(String[] args) {
+        // Scanner sc=new Scanner(System.in);
+        // int a=sc.nextInt(),b=sc.nextInt();
+		
+        // System.out.println(a+b);
+		// System.out.println("RJHd");
+		// System.out.println("李红亮");
+		
+    // }    
+// }
+
