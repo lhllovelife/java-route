93220ab5df99f75c996ecea53ed09a9d85ed264a
lhllovezj <lhllovezj@gmail.com>
2020-05-12 02:46:37 +0800

    一维数组

--- JavaSE(老杜)/ProjectSets/javase进阶/Array/src/ArrayTest01.java
+++ JavaSE(老杜)/ProjectSets/javase进阶/Array/src/ArrayTest01.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class ArrayTest01 {
-    Object
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[10];
+        int[] b = {15, 20, 0, 48};
+        int x;
+        for(int i = 0; i < 5; i++)
+        {
+            x = sc.nextInt();
+            a[i] = x;
+        }
+        for(int i = 0; i < 5; i++)
+        {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        String[] s = new String[0];
+        System.out.println(s[1]);
+    }
 }
